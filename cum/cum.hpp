8df8e8f747721692395626cd2a51189d5c51e269
therooftopprinz <mynameisrainer@gmail.com>
2020-05-26 18:08:20 +0800

    Get variant index by type helper.

--- cum/cum.hpp
+++ cum/cum.hpp
@@ -22,6 +22,23 @@ using DetermineUnsignedType =
         >
     >;
 
+template<typename VariantType, typename T, std::size_t index = 0>
+constexpr std::size_t GetIndexByType()
+{
+    if constexpr (index == std::variant_size_v<VariantType>)
+    {
+        return index;
+    }
+    else if constexpr (std::is_same_v<std::variant_alternative_t<index, VariantType>, T>)
+    {
+        return index;
+    }
+    else
+    {
+        return GetIndexByType<VariantType, T, index + 1>();
+    }
+}
+
 template<typename T, size_t N>
 struct vector : std::vector<T>
 {
